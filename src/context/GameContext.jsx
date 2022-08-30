bf12e1b370cdfc1b34ac505d367503352f56dff6
AaronLocatis <aalocatis2@gmail.com>
2022-08-30 09:25:55 -0500

    added cards, active players, and game status

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -2,12 +2,22 @@ import React, { createContext, useContext } from "react";
 
 export const GameContext = createContext(null);
 
+
 export function useGameContext() {
+
+  const [cards, setCards] = useState([]);
+  const [activePlayer, setActivePlayer] = useState({ name: null, host: null, team: null });
+  const [gameStatus, setGameStatus] = useState(null);
+
+
+
   return useContext(GameContext);
 }
 
 export function GameProvider(props) {
   return (
-    <GameContext.Provider value={{}}>{props.children}</GameContext.Provider>
+    <GameContext.Provider value={{ cards, serCards, activePlayer, setActivePlayer, setStatus, setGameStatus }}>{props.children}</GameContext.Provider>
   );
 }
+
+export default useGameContext
