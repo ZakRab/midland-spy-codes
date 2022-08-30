81192a052e4edec304a4bb10778c29bb8b474892
ZakRab <Zakomaha@gmail.com>
2022-08-30 09:29:04 -0500

    adds host key

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -2,24 +2,39 @@ import React, { createContext, useContext, useState } from "react";
 
 export const GameContext = createContext(null);
 
-
 export function useGameContext() {
-
   const [cards, setCards] = useState([]);
-  const [activePlayer, setActivePlayer] = useState({ name: null, host: null, team: null });
+  const [activePlayer, setActivePlayer] = useState({
+    name: null,
+    isHost: false,
+    team: null,
+  });
   const [gameStatus, setGameStatus] = useState(null);
   const [players, setPlayers] = useState([]);
   const [activeTeam, setActiveTeam] = useState(null);
-
-
 
   return useContext(GameContext);
 }
 
 export function GameProvider(props) {
   return (
-    <GameContext.Provider value={{ cards, serCards, activePlayer, setActivePlayer, setStatus, setGameStatus, players, setPlayers, activeTeam, setActiveTeam }}>{props.children}</GameContext.Provider>
+    <GameContext.Provider
+      value={{
+        cards,
+        serCards,
+        activePlayer,
+        setActivePlayer,
+        setStatus,
+        setGameStatus,
+        players,
+        setPlayers,
+        activeTeam,
+        setActiveTeam,
+      }}
+    >
+      {props.children}
+    </GameContext.Provider>
   );
 }
 
-export default useGameContext
+export default useGameContext;
