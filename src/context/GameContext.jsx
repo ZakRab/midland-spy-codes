52b7464792084b0ea05b7993b372da2caa174afd
ZakRab <Zakomaha@gmail.com>
2022-08-30 09:23:07 -0500

    adds state for players and active team

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 export const GameContext = createContext(null);
 
 export function useGameContext() {
+  const [players, setPlayers] = useState([]);
+  const [activeTeam, setActiveTeam] = useState(null);
+
   return useContext(GameContext);
 }
 
 export function GameProvider(props) {
   return (
-    <GameContext.Provider value={{}}>{props.children}</GameContext.Provider>
+    <GameContext.Provider
+      value={{ players, setPlayers, activeTeam, setActiveTeam }}
+    >
+      {props.children}
+    </GameContext.Provider>
   );
 }
