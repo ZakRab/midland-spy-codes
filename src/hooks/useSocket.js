462a05683879b600bb45b721624792b2cdf36c41
ZakRab <107758804+ZakRab@users.noreply.github.com>
2022-08-30 15:44:28 -0500

    sets up socket query handshake hook

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,1 +1,17 @@
-export default function useSocket() {}
+import { io } from "socket.io-client";
+import useGameContext from "../../src/context/GameContext";
+import { useEffect } from "react";
+
+export default function useSocket(lobby) {
+  const { activePlayer, players, setPlayers } = useGameContext();
+  const socketRef = useRef;
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000", {
+      query: {
+        displayName: activePlayer.name,
+        isHost: activePlayer.isHost,
+      },
+    });
+    socketRef.current.on("");
+  });
+}
