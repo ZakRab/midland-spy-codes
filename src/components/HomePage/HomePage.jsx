55dd703a0c895cef3f5420a6ef37591a9d342364
Aidee Hernandez <aideegh@live.com>
2022-08-31 09:09:11 -0500

    home buttons

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,61 +1,71 @@
 import React, { useMemo, useState } from "react";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import useGameContext from "../../context/GameContext"
+import useGameContext from "../../context/GameContext";
+import { Grid } from "@mui/material";
 
 function HomePage() {
   const navigate = useNavigate();
   const { setActivePlayer } = useGameContext();
-  // const lobbyError = useMemo(
-  //   () => lobby.length = 4
-  //   [lobby]
-  // );
-  const [name, setName] = useState("");
   const [lobby, setLobby] = useState("");
+  const [name, setName] = useState("");
+  const lobbyError = useMemo(() => lobby.length <= 3, [lobby]);
+  const nameError = useMemo(() => name.length < 0, [name]);
   return (
-    <div>
-      <h1 className="">Welcome to Codename</h1>
-      <div>
-        <TextField
-          id="Name"
-          label="Name"
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={3}>
+        <h1 className="">Welcome to Codename</h1>
+        <div>
+          <TextField
+            id="Name"
+            label="Name"
+            variant="outlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            id="Lobby"
+            label="Lobby"
+            variant="outlined"
+            value={lobby}
+            onChange={(e) => setLobby(e.target.value)}
+          />
+        </div>
+        <Button
           variant="outlined"
-          value={name}
-          onChange={(e) => setName((e.target.value))}
-        />
-      </div>
-      <div>
-        <TextField
-          id="Lobby"
-          label="Lobby"
+          disabled={nameError || lobbyError}
+          onClick={(e) => {
+            setActivePlayer({ name, isHost: false, team: null });
+            navigate(`/game/:${lobby}`);
+          }}
+        >
+          Go to Lobby
+        </Button>
+        <Button
           variant="outlined"
-          value={lobby}
-          onChange={(e) => setLobby(e.target.value)} />
-      </div>
-      <Button
-        variant="outlined"
-        // disabled={nameError || lobbyError}
-        onClick={(e) => {
-          setActivePlayer({ name, isHost: false, team: null })
-          navigate(`/game/:${lobby}`)
-        }
-        }
-      >Go to Lobby</Button>
-      <Button
-        variant="outlined"
-        // disabled={nameError || lobbyError}
-        onClick={(e) => {
-          let lobby = ((Math.random() + 1).toString(36).substring(7))
-          setActivePlayer({ name, isHost: true, team: null })
-          navigate(`/game/:${lobby}`)
-        }}
-      >Create Lobby</Button>
-      <footer>Created by 1&&0</footer>
-    </div >
+          disabled={nameError || lobbyError}
+          onClick={(e) => {
+            let lobby = (Math.random() + 1).toString(36).substring(7);
+            setActivePlayer({ name, isHost: true, team: null });
+            navigate(`/game/:${lobby}`);
+          }}
+        >
+          Create Lobby
+        </Button>
+        <footer>Created by 1&&0</footer>
+      </Grid>
+    </Grid>
   );
 }
 
 export default HomePage;
-
-
