4504b447ac1b4b0d471c6df8771c4a0f9556840e
ZakRab <107758804+ZakRab@users.noreply.github.com>
2022-08-31 09:44:29 -0500

    adds connection and host update socket

--- server/config/socket.conf.js
+++ server/config/socket.conf.js
@@ -1,3 +1,13 @@
-const io = require("socket.io")(server, {
-	cors: { origin: ["*", "http://localhost:3000"] },
-});
+import { io } from "socket.io-client";
+
+const socketConf = (io) => {
+  io.on("connection", (socket) => {
+    const { name, isHost, lobby } = socket.handshake.query;
+    isHost = isHost === "true";
+    socket.join(lobby);
+    io.to(lobby).emit("user connect", { name, isHost });
+  });
+  io.on("update players", (players) => {
+    io.to(lobby).emit("update players", players);
+  });
+};
--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -8,10 +8,25 @@ export default function useSocket(lobby) {
   useEffect(() => {
     socketRef.current = io("http://localhost:3000", {
       query: {
-        displayName: activePlayer.name,
+        name: activePlayer.name,
         isHost: activePlayer.isHost,
+        lobby,
       },
     });
-    socketRef.current.on("");
+
+    socketRef.current.on("user connect", ({ name, isHost }) => {
+      if (activePlayer.isHost) {
+        setPlayers((curr) => {
+          let newPlayers = [...curr, { name, isHost }];
+          socketRef.current.emit("update players", newPlayers);
+          return newPlayers;
+        });
+      }
+    });
+    socketRef.current.on("update players", (newPlayers) => {
+      if (!activePlayer.isHost) {
+        setPlayers(newPlayers);
+      }
+    });
   });
 }
