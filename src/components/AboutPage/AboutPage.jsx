4e1e1744ffd4a097afeb787e700d32b83ea9bfa1
Ethan Haneklaus <ethanhaneklaus@gmail.com>
2022-08-31 13:22:22 -0500

    spelling error

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -15,14 +15,14 @@ function AboutPage() {
 
   return (
     <>
-      <div>AboutPage</div>
-      <h1>Read about our project!</h1>
+      <h1>AboutPage</h1>
+      <h3>Read about our project!</h3>
       <p>
         First and for most, thank you for taking the time to look at our
         project! This group project was our final for The Midland Code Academy
         to show off our newly gained knowledge and skills developed in the
         program. We hope you enjoy playing over version of Codename that we have
-        worked very hard on. Bellow you will find small sections about each of
+        worked very hard on. Below you will find small sections about each of
         us and why we decide to get into coding as well as our Career goals.
         Please feel free to connect with us on our included social networks.
       </p>
