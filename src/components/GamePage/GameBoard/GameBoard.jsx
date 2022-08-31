b7cd99a0335f9762460b009bc456f907bbff1a2e
chegarty74 <chegarty74@yahoo.com>
2022-08-31 11:00:40 -0500

    Initial scaffold Game Board.

--- src/components/GamePage/GameBoard/GameBoard.jsx
+++ src/components/GamePage/GameBoard/GameBoard.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: "center",
+	color: theme.palette.text.secondary,
+}));
 
 function GameBoard() {
-  return <div>GameBoard</div>;
+   return (
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid
+					container
+					spacing={{ xs: 2, md: 3 }}
+					columns={{ xs: 1, sm: 8, md: 12 }}
+				>
+					{Array.from(Array(16)).map((_, index) => (
+						<Grid item xs={2} sm={3} md={3} key={index}>
+							<Item>
+                <p>Card component goes in here.</p>
+                </Item>
+						</Grid>
+					))}
+				</Grid>
+			</Box>
+		);
 }
 
 export default GameBoard;
