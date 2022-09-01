d3c91e59653d491e41d0245c8c334bc946f1f6d7
Mike Truax <mike@miketruax.com>
2022-09-01 11:34:46 -0500

    updates styling / classes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
 } from "react-router-dom";
 import AboutPage from "./components/AboutPage/AboutPage";
 import GamePage from "./components/GamePage/GamePage";
 import RulesPage from "./components/RulesPage/RulesPage";
 import HomePage from "./components/HomePage/HomePage";
 import Menu from "./components/Menu";
+import { Typography } from "@mui/material";
 
 const App = () => {
-	return (
-		<Router>
-			<Menu />
-			<Routes>
-				<Route exact path="/about" element={<AboutPage />}></Route>
-				<Route exact path="/game/:lobby" element={<GamePage />}></Route>
-				<Route exact path="/rules" element={<RulesPage />}></Route>
-				<Route exact path="/home" element={<HomePage />}></Route>
-				<Route exact path="*" element={<Navigate to="/home" />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Menu />
+      <Routes>
+        <Route exact path="/about" element={<AboutPage />}></Route>
+        <Route exact path="/game/:lobby" element={<GamePage />}></Route>
+        <Route exact path="/rules" element={<RulesPage />}></Route>
+        <Route exact path="/home" element={<HomePage />}></Route>
+        <Route exact path="*" element={<Navigate to="/home" />} />
+      </Routes>
+      <Typography mt={2} variant="h6" align={"center"}>
+        Created by 1&&0
+      </Typography>
+    </Router>
+  );
 };
 
 export default App;
