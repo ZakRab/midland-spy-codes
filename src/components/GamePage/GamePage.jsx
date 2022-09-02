3207713557f14301a6fc018202060c99ea75c67b
AaronLocatis <aalocatis2@gmail.com>
2022-09-02 16:55:19 -0500

    color changes

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -30,17 +30,10 @@ function GamePage() {
   );
   return (
     <div>
-      <div className="">
-        <img src="https://czechgames.com/for-press/codenames/codenames-13.png"></img>
-      </div>
-      <TeamSelect joinTeam={joinTeam} sendCards={sendCards} players={players} />
-      {gameStatus == "started" && <Clue sendClue={sendClue}></Clue>}
-      {gameStatus == "started" && (
-        <GameBoard sendSelectedCard={sendSelectedCard}></GameBoard>
-      )}
-
       <Typography variant="h3" align="center" m={2}>
-        Game
+        <div className="picture1">
+          <img src="https://czechgames.com/for-press/codenames/codenames-13.png"></img>
+        </div>
       </Typography>
       <Grid
         container
