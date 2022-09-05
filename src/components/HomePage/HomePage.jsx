efcce3e5e183835b99aa1e5db9bf124c1f02346f
AaronLocatis <aalocatis2@gmail.com>
2022-09-05 13:41:11 -0500

    Color and styling changes

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -134,7 +134,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import useGameContext from "../../context/GameContext";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -148,78 +148,93 @@ function HomePage() {
   );
 
   return (
-    // <Box
-    //   component="img"
-    //   sx={{
-    //     height: 250,
-    //     width: 250,
-    //     backgroundRepeat: "no-repeat",
-    //     backgroundPosition: "right",
-    //   }}
-    //   src="https://www.pinclipart.com/picdir/big/1-14882_detective-clipart-secret-agent-clipart-silhouette-png-download.png"
-    // >
-    <Grid
-      textAlign="center"
-      container
-      direction="row"
-      justifyContent="center"
-      rowSpacing={2}
-      alignItems="center"
-    >
-      <Grid item xs={12}>
-        <div className="">
-          <img src="https://czechgames.com/for-press/codenames/codenames-13.png"></img>
-        </div>
+    <div>
+      <Grid
+        textAlign="center"
+        container
+        direction="row"
+        justifyContent="center"
+        rowSpacing={2}
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <div className="">
+            <img src="https://czechgames.com/for-press/codenames/codenames-13.png"></img>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="Name"
+            label="Name"
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              color: "#212121",
+              backgroundColor: "#212121",
+            }}
+            value={name}
+            error={nameError}
+            helperText="Must be less than 10 characters"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="Lobby"
+            label="Lobby"
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              color: "#212121",
+              backgroundColor: "#212121",
+            }}
+            value={lobby}
+            error={lobbyError}
+            helperText="Must be 5 characters"
+            onChange={(e) => setLobby(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              color: "#d50000",
+              backgroundColor: "#212121",
+            }}
+            disabled={nameError || lobbyError}
+            onClick={(e) => {
+              setActivePlayer({ name, isHost: false, team: null, role: null });
+              navigate(`/game/${lobby}`);
+            }}
+          >
+            Go to Lobby
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              color: "#d50000",
+              backgroundColor: "#212121",
+            }}
+            disabled={nameError || !(!nameError && lobbyError)}
+            onClick={(e) => {
+              let lobby = (Math.random() + 1).toString(36).substring(7);
+              setActivePlayer({ name, isHost: true, team: null, role: null });
+              navigate(`/game/${lobby}`);
+            }}
+          >
+            Create Lobby
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <TextField
-          id="Name"
-          label="Name"
-          variant="outlined"
-          value={name}
-          error={nameError}
-          helperText="Must be less than 10 characters"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          id="Lobby"
-          label="Lobby"
-          variant="outlined"
-          value={lobby}
-          error={lobbyError}
-          helperText="Must be 5 characters"
-          onChange={(e) => setLobby(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          variant="outlined"
-          disabled={nameError || lobbyError}
-          onClick={(e) => {
-            setActivePlayer({ name, isHost: false, team: null, role: null });
-            navigate(`/game/${lobby}`);
-          }}
-        >
-          Go to Lobby
-        </Button>
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          variant="outlined"
-          disabled={nameError || !(!nameError && lobbyError)}
-          onClick={(e) => {
-            let lobby = (Math.random() + 1).toString(36).substring(7);
-            setActivePlayer({ name, isHost: true, team: null, role: null });
-            navigate(`/game/${lobby}`);
-          }}
-        >
-          Create Lobby
-        </Button>
-      </Grid>
-    </Grid>
-    // </Box>
+      <div className="picture1">
+        /{" "}
+        <img src="https://www.pinclipart.com/picdir/big/1-14882_detective-clipart-secret-agent-clipart-silhouette-png-download.png"></img>
+      </div>
+    </div>
   );
 }
 
