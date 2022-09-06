959d9d831f0e6a99933effd09dd571b295c46f03
chegarty74 <chegarty74@yahoo.com>
2022-09-06 10:34:10 -0500

    Added border to selected card, other details

--- src/components/GamePage/GameBoard/Card.jsx
+++ src/components/GamePage/GameBoard/Card.jsx
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import useGameContext from "../../../context/GameContext";
 
 function Card({ card }) {
-  const { activePlayer, setSelectedCard, selectedCard } = useGameContext();
+	const { activePlayer, setSelectedCard, selectedCard } = useGameContext();
 
-  return (
-    <div onClick={() => setSelectedCard(card)}>
-      {activePlayer.role === "spymaster" && (
-        <div style={{ backgroundColor: card.color }}>
-          {!card.isFaceUp && <p>{card.word}</p>}
-        </div>
-      )}
-      {activePlayer.role === "operative" && (
-        <div>{!card.isFaceUp && <p>{card.word}</p>}</div>
-      )}
-      {selectedCard.word}
-    </div>
-  );
+	return (
+		<div onClick={() => setSelectedCard(card)}>
+			{activePlayer.role === "spymaster" && (
+				<div style={{ backgroundColor: card.color }}>
+					{!card.isFaceUp && <p>{card.word}</p>}
+				</div>
+			)}
+			{activePlayer.role === "operative" && selectedCard !== card && (
+				<div>{!card.isFaceUp && <p>{card.word}</p>}</div>
+			)}
+			{activePlayer.role === "operative" && selectedCard === card && (
+				<div style={{ border: "2px solid green" }}>
+					{!card.isFaceUp && <p>{card.word}</p>}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Card;
--- src/components/GamePage/GameBoard/GameBoard.jsx
+++ src/components/GamePage/GameBoard/GameBoard.jsx
@@ -7,44 +7,51 @@ import useGameContext from "../../../context/GameContext";
 import Card from "./Card";
 
 function GameBoard({ sendSelectedCard }) {
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: "5px",
-    margin: "5px",
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-  const { activePlayer, setSelectedCard, activeTeam, selectedCard, cards } =
-    useGameContext();
-  return (
-    <>
-      <Grid
-        justifyContent="center"
-        container
-        // spacing={{ xs: 0, md: 1 }}
-        columns={{ xs: 1, sm: 9, md: 12 }}
-      >
-        {cards.map((card, index) => (
-          <Grid flexGrow={1} item xs={2} sm={3} md={3} key={index}>
-            <Item onClick={() => setSelectedCard(card)}>
-              <Card card={card}></Card>
-            </Item>
-          </Grid>
-        ))}
-      </Grid>
-      {activePlayer.role === "operative" && activePlayer.team === activeTeam && (
-        <Button
-          variant="contained"
-          onClick={() => {
-            sendSelectedCard(selectedCard);
-          }}
-        >
-          Flip Card
-        </Button>
-      )}
-    </>
-  );
+	const Item = styled(Paper)(({ theme }) => ({
+		backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+		...theme.typography.body2,
+		padding: "5px",
+		margin: "5px",
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+	}));
+	const { activePlayer, setSelectedCard, activeTeam, selectedCard, cards } =
+		useGameContext();
+	return (
+		<>
+			<Grid
+				justifyContent="center"
+				container
+				// spacing={{ xs: 0, md: 1 }}
+				columns={{ xs: 1, sm: 9, md: 12 }}
+			>
+				{cards.map((card, index) => (
+					<Grid flexGrow={1} item xs={2} sm={3} md={3} key={index}>
+						<Item
+							sx={{ cursor: "pointer" }}
+							onClick={() =>
+								card === selectedCard
+									? setSelectedCard(null)
+									: setSelectedCard(card)
+							}
+						>
+							<Card card={card}></Card>
+						</Item>
+					</Grid>
+				))}
+			</Grid>
+			{activePlayer.role === "operative" && activePlayer.team === activeTeam && (
+				<Button
+					variant="contained"
+					onClick={() => {
+						sendSelectedCard(selectedCard);
+					}}
+				>
+					Flip Card
+				</Button>
+			)}
+		</>
+	);
 }
 
 export default GameBoard;
