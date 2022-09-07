e6a4cb864a44050debaa5076ad28fe0a0f097aba
chegarty74 <chegarty74@yahoo.com>
2022-09-07 10:35:11 -0500

    Initial card styling - Add Poppins - adjust some default colors.

--- src/components/GamePage/GameBoard/Card.jsx
+++ src/components/GamePage/GameBoard/Card.jsx
@@ -2,32 +2,49 @@ import React, { useState } from "react";
 import useGameContext from "../../../context/GameContext";
 
 function Card({ card }) {
-  const { activePlayer, setSelectedCard, selectedCard } = useGameContext();
-
-  return (
-    <div onClick={() => setSelectedCard(card)}>
-      {activePlayer.role === "spymaster" && (
-        <div style={{ backgroundColor: card.color }}>
-          {!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}
-        </div>
-      )}
-      {activePlayer.role === "operative" && selectedCard !== card && (
-        <div>{!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}</div>
-      )}
-      {activePlayer.role === "operative" &&
-        selectedCard === card &&
-        !card.isFaceUp && (
-          <div style={{ border: "2px solid green" }}>
-            {!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}
-          </div>
-        )}
-      {activePlayer.role === "operative" && card.isFaceUp && (
-        <div style={{ backgroundColor: card.color }}>
-          {!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}
-        </div>
-      )}
-    </div>
-  );
+	const { activePlayer, setSelectedCard, selectedCard } = useGameContext();
+	const spyStyle = {
+		backgroundColor: card.color,
+		aspectRatio: "1/1",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		fontSize: "1.5em",
+		color: "#fff",
+		textTransform: "uppercase",
+		fontFamily: "Poppins, sans serif",
+	};
+	const opStyle = {
+		aspectRatio: "1/1",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		fontSize: "1.5em",
+		color: "#212020",
+		textTransform: "uppercase",
+	};
+	return (
+		<div onClick={() => setSelectedCard(card)}>
+			{activePlayer.role === "spymaster" && (
+				<div style={spyStyle}>
+					{!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}
+				</div>
+			)}
+			{activePlayer.role === "operative" && selectedCard !== card && (
+				<div style={opStyle}>
+					{!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}
+				</div>
+			)}
+			{activePlayer.role === "operative" &&
+				selectedCard === card &&
+				!card.isFaceUp && (
+					<div>{!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}</div>
+				)}
+			{activePlayer.role === "operative" && card.isFaceUp && (
+				<div>{!card.isFaceUp ? <p>{card.word}</p> : <p>fliped</p>}</div>
+			)}
+		</div>
+	);
 }
 
 export default Card;
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -4,83 +4,83 @@ import React, { createContext, useContext, useState, useCallback } from "react";
 export const GameContext = createContext(null);
 
 export default function useGameContext() {
-  return useContext(GameContext);
+	return useContext(GameContext);
 }
 
 export function GameProvider(props) {
-  const [cards, setCards] = useState([]);
-  const [activePlayer, setActivePlayer] = useState({
-    name: null,
-    isHost: false,
-    team: null,
-    role: null,
-  });
-  const [gameStatus, setGameStatus] = useState(null);
-  const [players, setPlayers] = useState([]);
-  const [winningTeam, setWinningTeam] = useState(null);
-  const [clue, setClue] = useState("");
+	const [cards, setCards] = useState([]);
+	const [activePlayer, setActivePlayer] = useState({
+		name: null,
+		isHost: false,
+		team: null,
+		role: null,
+	});
+	const [gameStatus, setGameStatus] = useState(null);
+	const [players, setPlayers] = useState([]);
+	const [winningTeam, setWinningTeam] = useState(null);
+	const [clue, setClue] = useState("");
 
-  const createCards = useCallback((words) => {
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
+	const createCards = useCallback((words) => {
+		function shuffleArray(array) {
+			for (let i = array.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[array[i], array[j]] = [array[j], array[i]];
+			}
+		}
 
-    let cards = words.map((word, idx) => {
-      let type;
-      let color;
-      if (idx <= 5) {
-        type = "red";
-        color = "red";
-      } else if (idx <= 11) {
-        type = "blue";
-        color = "blue";
-      } else if (idx === 12) {
-        type = "bomb";
-        color = "gray";
-      } else {
-        type = "neutral";
-        color = "beige";
-      }
-      return {
-        word,
-        type,
-        color,
-        isFaceUp: false,
-      };
-    });
+		let cards = words.map((word, idx) => {
+			let type;
+			let color;
+			if (idx <= 5) {
+				type = "red";
+				color = "red";
+			} else if (idx <= 11) {
+				type = "blue";
+				color = "blue";
+			} else if (idx === 12) {
+				type = "bomb";
+				color = "#2b2929";
+			} else {
+				type = "neutral";
+				color = "#a24cae";
+			}
+			return {
+				word,
+				type,
+				color,
+				isFaceUp: false,
+			};
+		});
 
-    shuffleArray(cards);
-    setCards(cards);
-    return cards;
-  }, []);
-  const [activeTeam, setActiveTeam] = useState("red");
-  const [selectedCard, setSelectedCard] = useState({});
-  return (
-    <GameContext.Provider
-      value={{
-        cards,
-        setCards,
-        activePlayer,
-        setActivePlayer,
-        gameStatus,
-        setGameStatus,
-        players,
-        setPlayers,
-        activeTeam,
-        setActiveTeam,
-        setWinningTeam,
-        winningTeam,
-        clue,
-        setClue,
-        createCards,
-        setSelectedCard,
-        selectedCard,
-      }}
-    >
-      {props.children}
-    </GameContext.Provider>
-  );
+		shuffleArray(cards);
+		setCards(cards);
+		return cards;
+	}, []);
+	const [activeTeam, setActiveTeam] = useState("red");
+	const [selectedCard, setSelectedCard] = useState({});
+	return (
+		<GameContext.Provider
+			value={{
+				cards,
+				setCards,
+				activePlayer,
+				setActivePlayer,
+				gameStatus,
+				setGameStatus,
+				players,
+				setPlayers,
+				activeTeam,
+				setActiveTeam,
+				setWinningTeam,
+				winningTeam,
+				clue,
+				setClue,
+				createCards,
+				setSelectedCard,
+				selectedCard,
+			}}
+		>
+			{props.children}
+		</GameContext.Provider>
+	);
 }
