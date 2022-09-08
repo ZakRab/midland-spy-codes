e75bd8383daffa3adc3aa7ef26247a170e383811
ZakRab <107758804+ZakRab@users.noreply.github.com>
2022-09-08 09:55:59 -0500

    adds predeployment functionality

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(express.json());
 app.use(express.static(__dirname + "/build"));
 socketConf(io);
 
+if (process.env.NODE_ENV === "production") {
+  app.enable("trust proxy");
+  app.use((req, res, next) => {
+    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+  });
+}
+
 app.get("*", (req, res) => {
   return res.sendFile("/build/index.html", { root: __dirname + "/" });
 });
