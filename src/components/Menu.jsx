642bdcd0710d635005747579a823223e21815d91
chegarty74 <chegarty74@yahoo.com>
2022-09-09 14:46:48 -0500

    Initial click to copy setup

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,6 +8,8 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import ClickToCopy from "../shared/functions/ClickToCopy";
+import lobbyCode from "../shared/functions/lobbyCode";
 
 import Button from "@mui/material/Button";
 
@@ -19,137 +21,142 @@ import spyIcon from "../shared/icons/spy-icon.svg";
 import { Grid } from "@mui/material";
 
 const pages = [
-  { text: "Home", path: "/home", icon: HomeIcon },
-  { text: "Rules", path: "/rules", icon: ListAltIcon },
-  { text: "About", path: "/about", icon: InfoIcon },
+	{ text: "Home", path: "/home", icon: HomeIcon },
+	{ text: "Rules", path: "/rules", icon: ListAltIcon },
+	{ text: "About", path: "/about", icon: InfoIcon },
 ];
 
 function NavMenu() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  // const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	// const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            component={NavLink}
-            to="/home"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <img className="spy-icon" src={spyIcon} alt="spy-icon" />
-          </Typography>
+	let lobby = lobbyCode(5);
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {/* mobile dropdown menu */}
-              {pages.map((page) => (
-                <MenuItem
-                  component={NavLink}
-                  to={page.path}
-                  key={page.text}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">
-                    <page.icon
-                      style={{ verticalAlign: "middle", paddingRight: "3px" }}
-                    />
-                    {page.text}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component={NavLink}
-            to="/home"
-            // component="a"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <img className="spy-icon" src={spyIcon} alt="spy-icon" />
-          </Typography>
+	return (
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						component={NavLink}
+						to="/home"
+						noWrap
+						sx={{
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+						}}
+					>
+						<img className="spy-icon" src={spyIcon} alt="spy-icon" />
+					</Typography>
 
-          {/* desktop links: */}
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							{/* mobile dropdown menu */}
+							{pages.map((page) => (
+								<MenuItem
+									component={NavLink}
+									to={page.path}
+									key={page.text}
+									onClick={handleCloseNavMenu}
+								>
+									<Typography textAlign="center">
+										<page.icon
+											style={{ verticalAlign: "middle", paddingRight: "3px" }}
+										/>
+										{page.text}
+									</Typography>
+								</MenuItem>
+							))}
+							<MenuItem>
+								<ClickToCopy copyText={lobby}></ClickToCopy>
+							</MenuItem>
+						</Menu>
+					</Box>
+					<Typography
+						variant="h5"
+						noWrap
+						component={NavLink}
+						to="/home"
+						// component="a"
+						sx={{
+							mr: 2,
+							display: { xs: "flex", md: "none" },
+							flexGrow: 1,
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+						}}
+					>
+						<img className="spy-icon" src={spyIcon} alt="spy-icon" />
+					</Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                component={NavLink}
-                to={page.path}
-                key={page.text}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                <page.icon
-                  style={{ verticalAlign: "middle", paddingRight: "3px" }}
-                />
-                {page.text}
-              </Button>
-            ))}
-          </Box>
-          {/* <Grid item xs={12}>
+					{/* desktop links: */}
+
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{pages.map((page) => (
+							<Button
+								component={NavLink}
+								to={page.path}
+								key={page.text}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "white", display: "block" }}
+							>
+								<page.icon
+									style={{ verticalAlign: "middle", paddingRight: "3px" }}
+								/>
+								{page.text}
+							</Button>
+						))}
+					</Box>
+					{/* <Grid item xs={12}>
             <h1 className="game-title2">SPY CODE</h1>
           </Grid> */}
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 
 export default NavMenu;
---
+++ src/shared/functions/ClickToCopy.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Grid, Typography } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+function ClickToCopy({ copyText }) {
+	const [isCopied, setIsCopied] = useState(false);
+
+	async function copyTextToClipboard(text) {
+		if ("clipboard" in navigator) {
+			return await navigator.clipboard.writeText(text);
+		} else {
+			return document.execCommand("copy", true, text);
+		}
+	}
+
+	// onClick handler function for the copy button
+	const handleCopyClick = () => {
+		// Asynchronously call copyTextToClipboard
+		copyTextToClipboard(copyText)
+			.then(() => {
+				// If successful, update the isCopied state value
+				setIsCopied(true);
+				setTimeout(() => {
+					setIsCopied(false);
+				}, 1500);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<Grid>
+			<Typography>{copyText}</Typography>
+			<ContentCopyIcon></ContentCopyIcon>
+		</Grid>
+	);
+}
+
+export default ClickToCopy;
