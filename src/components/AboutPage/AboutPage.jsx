836da946dc6dbfcc776416d4c1a839ec594cdea8
Donella Cooper <donellawc@gmail.com>
2022-09-09 14:14:45 -0500

    add flex and move card on about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -18,11 +18,13 @@ function AboutPage() {
           alignItems="center"
           align={"center"}
           padding="40px"
+          display="flex"
         >
           <div className="background-card bg-white text-black about-width">
             <Typography
               mt={2}
               mb={2}
+
               variant="h4"
               align={"center"}
               sx={{ marginTop: 0 }}
@@ -36,7 +38,7 @@ function AboutPage() {
               project! This group project was our final for The Midland Code
               Academy to show off our newly gained knowledge and skills developed
               in the program. We hope you enjoy playing our game Spy Code
-              that we have worked very hard on. Below you will find small bios
+              that we have worked very hard on. Below you will find bios
               about each of us and why we decided to get into coding as well as
               our career goals. Please feel free to connect with us on our
               included social networks.
