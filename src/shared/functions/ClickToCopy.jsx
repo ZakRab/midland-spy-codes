9994a3d69574fe40d0d1d61f6a1f63a693ea31fa
Mike Truax <mike@miketruax.com>
2022-09-09 15:56:31 -0500

    adds copy code

--- src/shared/functions/ClickToCopy.jsx
+++ src/shared/functions/ClickToCopy.jsx
@@ -1,38 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { Grid, Typography } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function ClickToCopy({ copyText }) {
-	const [isCopied, setIsCopied] = useState(false);
+  async function copyTextToClipboard(text) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    }
+  }
 
-	async function copyTextToClipboard(text) {
-		if ("clipboard" in navigator) {
-			return await navigator.clipboard.writeText(text);
-		}
-	}
+  // onClick handler function for the copy button
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(copyText)
+      .then(() => {
+        // If successful, update the isCopied state value
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	// onClick handler function for the copy button
-	const handleCopyClick = () => {
-		// Asynchronously call copyTextToClipboard
-		copyTextToClipboard(copyText)
-			.then(() => {
-				// If successful, update the isCopied state value
-				setIsCopied(true);
-				setTimeout(() => {
-					setIsCopied(false);
-				}, 1500);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
-	return (
-		<Grid sx={{ display: "flex" }}>
-			<Typography>{copyText}</Typography>
-			<ContentCopyIcon></ContentCopyIcon>
-		</Grid>
-	);
+  return (
+    <Grid sx={{ display: "flex" }} onClick={handleCopyClick}>
+      <Typography>{copyText}</Typography>
+      <ContentCopyIcon></ContentCopyIcon>
+    </Grid>
+  );
 }
 
 export default ClickToCopy;
